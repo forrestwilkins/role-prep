dcac36494b4f6dce3bd70c468b16cfde6f22c14e
forrestwilkins <ethanforrestwilkins@gmail.com>
2021-11-27 09:17:20 -0500

    Added ormconfig and scripts

--- src/backend/app.module.ts
+++ src/backend/app.module.ts
@@ -1,26 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { DEFAULT_DB, Environments } from "../constants/common";
-
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
-import { CommentsModule } from "./comments/comments.module";
 import { PostsModule } from "./posts/posts.module";
 import { UsersModule } from "./users/users.module";
+import { CommentsModule } from "./comments/comments.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: DEFAULT_DB,
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [],
-      synchronize: process.env.NODE_ENV === Environments.Development,
-    }),
-
+    TypeOrmModule.forRoot(),
     AuthModule,
     UsersModule,
     PostsModule,
