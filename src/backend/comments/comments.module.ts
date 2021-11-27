8801d7eca96fd11264f82a073bdc49556e2338fc
forrestwilkins <ethanforrestwilkins@gmail.com>
2021-11-27 14:24:05 -0500

    e2e tests pass

--- src/backend/comments/comments.module.ts
+++ src/backend/comments/comments.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CommentEntity } from "./comment.entity";
 import { CommentsController } from "./comments.controller";
 import { CommentsService } from "./comments.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CommentEntity])],
   controllers: [CommentsController],
   providers: [CommentsService],
-  exports: [CommentsService],
+  exports: [CommentsService, TypeOrmModule],
 })
 export class CommentsModule {}
