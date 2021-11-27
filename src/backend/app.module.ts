3c7e4574bd9b49eac8592bc2adce2ba82b9eecc4
forrestwilkins <ethanforrestwilkins@gmail.com>
2021-11-27 17:29:33 -0500

    Ormconfig added back

--- src/backend/app.module.ts
+++ src/backend/app.module.ts
@@ -5,21 +5,11 @@ import { AuthModule } from "./auth/auth.module";
 import { PostsModule } from "./posts/posts.module";
 import { UsersModule } from "./users/users.module";
 import { CommentsModule } from "./comments/comments.module";
-import { DEFAULT_DB, Environments } from "../constants/common";
+import config from "./ormconfig";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: DEFAULT_DB,
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV === Environments.Development,
-      keepConnectionAlive: true,
-      autoLoadEntities: true,
-    }),
+    TypeOrmModule.forRoot(config),
 
     AuthModule,
     UsersModule,
---
+++ src/backend/ormconfig.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DEFAULT_DB, Environments } from "../constants/common";
+
+const config: TypeOrmModuleOptions = {
+  type: DEFAULT_DB,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["dist/src/**/*.entity{.ts,.js}"],
+  synchronize: process.env.NODE_ENV === Environments.Development,
+  keepConnectionAlive: true,
+  autoLoadEntities: true,
+};
+
+export default config;
