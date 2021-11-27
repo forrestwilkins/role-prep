e71ee68bd40379240105d4cdc1e154c97b74a1d5
forrestwilkins <ethanforrestwilkins@gmail.com>
2021-11-27 15:30:51 -0500

    App starts, but crashes due to issues with entities

--- src/backend/app.module.ts
+++ src/backend/app.module.ts
@@ -5,10 +5,22 @@ import { AuthModule } from "./auth/auth.module";
 import { PostsModule } from "./posts/posts.module";
 import { UsersModule } from "./users/users.module";
 import { CommentsModule } from "./comments/comments.module";
+import { DEFAULT_DB, Environments } from "../constants/common";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: DEFAULT_DB,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV === Environments.Development,
+      keepConnectionAlive: true,
+      autoLoadEntities: true,
+    }),
+
     AuthModule,
     UsersModule,
     PostsModule,
--- src/backend/users/users.module.ts
+++ src/backend/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostsModule } from "../posts/posts.module";
 import { UsersController } from "./users.controller";
+import { PostEntity } from "../posts/post.entity";
 import { UsersService } from "./users.service";
 import { UserEntity } from "./user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), PostsModule],
+  imports: [TypeOrmModule.forFeature([UserEntity, PostEntity]), PostsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService, TypeOrmModule],
